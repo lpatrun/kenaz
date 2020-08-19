90461ad75865752149e76b4e74f7f5a361ca59d2
unknown <luka.patrun@gmail.com>
2020-08-19 17:55:03 +0200

    Initial dist subtree commit

---
+++ dist/precache-manifest.96dca800389dcd4fa7a5ce3fe9d1fff7.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d41a9bb27f6bb9818d34",
+    "url": "/kenaz/css/app.f91cc3c1.css"
+  },
+  {
+    "revision": "9559dc0e7cd7272e258603ec0cd9b562",
+    "url": "/kenaz/img/dribble.jpg"
+  },
+  {
+    "revision": "3916f350a438c5a16db0714d55f030b3",
+    "url": "/kenaz/img/facebook.jpg"
+  },
+  {
+    "revision": "f23989c90e5f0e0fa14013e740710ef5",
+    "url": "/kenaz/img/kenaz.png"
+  },
+  {
+    "revision": "40b2c0670de19c8be865212cbaa6a127",
+    "url": "/kenaz/img/linkedin.jpg"
+  },
+  {
+    "revision": "196159e54325f4849ea0804c87ea2ffe",
+    "url": "/kenaz/img/rss.jpg"
+  },
+  {
+    "revision": "3d97671152080da96eda27721420c423",
+    "url": "/kenaz/img/search.png"
+  },
+  {
+    "revision": "222c61ede180ce4cb845b561da694974",
+    "url": "/kenaz/img/skype.jpg"
+  },
+  {
+    "revision": "5cff6acf5562685107e6428e91b4ae7e",
+    "url": "/kenaz/img/twitter.jpg"
+  },
+  {
+    "revision": "088b7e0685b4338a2e87eff52b892589",
+    "url": "/kenaz/img/youtube.jpg"
+  },
+  {
+    "revision": "a943cee6c96ae64843d3769161d6b719",
+    "url": "/kenaz/index.html"
+  },
+  {
+    "revision": "d41a9bb27f6bb9818d34",
+    "url": "/kenaz/js/app.f772b293.js"
+  },
+  {
+    "revision": "dfde26cb3bb71ce2e869",
+    "url": "/kenaz/js/chunk-2d0a427a.19cd6834.js"
+  },
+  {
+    "revision": "1c84ac862a5bc7180028",
+    "url": "/kenaz/js/chunk-2d0b385f.9aa9f9c0.js"
+  },
+  {
+    "revision": "ee8e1a8d15560ffb6792",
+    "url": "/kenaz/js/chunk-2d0b91b8.c3c84d53.js"
+  },
+  {
+    "revision": "a39178ab73b4a8095f47",
+    "url": "/kenaz/js/chunk-2d0c831d.555b80cc.js"
+  },
+  {
+    "revision": "3992abeb64baf92a57f4",
+    "url": "/kenaz/js/chunk-2d0c976a.306fce81.js"
+  },
+  {
+    "revision": "bcea65ed7b98dd189fda",
+    "url": "/kenaz/js/chunk-2d0d0045.d691a382.js"
+  },
+  {
+    "revision": "66aea0f627473124ed94",
+    "url": "/kenaz/js/chunk-2d0d621e.7648b912.js"
+  },
+  {
+    "revision": "9eb21f0144692c3ae0de",
+    "url": "/kenaz/js/chunk-2d0d7a64.4d0ca809.js"
+  },
+  {
+    "revision": "1bc316d7505ad0b08065",
+    "url": "/kenaz/js/chunk-2d0df218.c004c57c.js"
+  },
+  {
+    "revision": "c02b12b46563cc05785c",
+    "url": "/kenaz/js/chunk-2d0df3e7.2e6b563e.js"
+  },
+  {
+    "revision": "7c6638745e6010140e67",
+    "url": "/kenaz/js/chunk-2d0e2711.7dd144b2.js"
+  },
+  {
+    "revision": "e043ab83c91e0bd9bb20",
+    "url": "/kenaz/js/chunk-2d0e4c2e.a858020c.js"
+  },
+  {
+    "revision": "aa49b02814d916c1aa9d",
+    "url": "/kenaz/js/chunk-2d0f0ffe.66b31006.js"
+  },
+  {
+    "revision": "9df7e7fa23f89ab05b21",
+    "url": "/kenaz/js/chunk-2d20ec20.4c9ee0c7.js"
+  },
+  {
+    "revision": "9cd6b3f98ff739725dab",
+    "url": "/kenaz/js/chunk-2d212c61.38512f41.js"
+  },
+  {
+    "revision": "b2ae5a098c5d675005ad",
+    "url": "/kenaz/js/chunk-2d217af9.1b930c85.js"
+  },
+  {
+    "revision": "4ae4f00a015551103584",
+    "url": "/kenaz/js/chunk-2d231206.f739705d.js"
+  },
+  {
+    "revision": "89fe15cb60f505db704b",
+    "url": "/kenaz/js/chunk-vendors.d928aeee.js"
+  },
+  {
+    "revision": "c6fa8cd532395c7514593e2d1ac9436c",
+    "url": "/kenaz/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/kenaz/robots.txt"
+  }
+]);
